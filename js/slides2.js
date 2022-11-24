c42510dec7b9c074caba986b5c9b9ac655c5cbf2
juan9900 <juanluislauretta@gmail.com>
2022-11-24 12:10:52 -0400

    agregado boton para descargar pdf en promo eventos

--- js/slides2.js
+++ js/slides2.js
@@ -2,7 +2,10 @@ document.addEventListener( 'DOMContentLoaded', function () {
 	new Splide( '#card-slider', {
 		perPage    : 3,
 		breakpoints: {
-			800: {
+			600: {
+				perPage: 1,
+			},
+			850:{
 				perPage: 1,
 			},
 			1024: {
